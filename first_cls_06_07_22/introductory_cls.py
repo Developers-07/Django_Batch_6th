94ba24b5487fa1eac7df468333c103a414c36d0b
Zishan Chowdhury <zishanc901@gmail.com>
2022-07-06 23:57:28 +0600

    first class

--- first_cls_06_07_22/introductory_cls.py
+++ first_cls_06_07_22/introductory_cls.py
@@ -1,1 +1,9 @@
 print("Welcome to Django 6th batch")
+
+# VARIABLE(which store value or data)
+# no symbol is allow(@,!,$,%,&) anywhere in variable
+# no numeric num is allow at first(1,2,3......) but you can use it at last point
+
+num=1000
+print("Numerical Number is :",num)
+print("Address Number is =",id(num))
