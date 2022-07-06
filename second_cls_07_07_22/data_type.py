7ec93785be5f506a9dc9a90fcec1842423f169c9
Zishan Chowdhury <zishanc901@gmail.com>
2022-07-07 02:11:43 +0600

    data_type

---
+++ second_cls_07_07_22/data_type.py
@@ -0,0 +1,13 @@
+# Premitive and Non-Premitive data type
+
+# Premitive -> (int,float,bool)
+# Non-Premitive -> (string,array)
+int=10
+float=20.87
+bool1=True
+bool2=False
+
+str="Welcome to Django Course"
+
+print(int,float,bool1,bool2,str)
+print(type(int),type(float),type(bool1),type(bool2),type(str))
